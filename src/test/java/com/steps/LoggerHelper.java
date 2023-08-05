0c8c30f1b182aea047446112adbcf508032497a6
Wesley <wesley-is-here@hotmail.com>
2023-08-05 17:58:14 +0800

    Commit

---
+++ src/test/java/com/steps/LoggerHelper.java
@@ -0,0 +1,24 @@
+package com.steps;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+/**
+ * 
+ * @author 
+ *
+ */
+public class LoggerHelper {
+
+	private static boolean root=false;
+	
+	public static Logger getLogger(Class cls){
+		if(root){
+			return Logger.getLogger(cls);
+		}
+		PropertyConfigurator.configure("log4j.properties");
+		root = true;
+		return Logger.getLogger(cls);
+	}
+	
+
+}
